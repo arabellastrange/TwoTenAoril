63afe9ecf42f4f6023a5630b299eeb14fe6dc2d0
AHC <AHC>
2017-02-02 20:15:22 +0000

    stage one and some of stage 2, first commit!

---
+++ shell2.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<string.h>
+
+char *words[50];
+
+void input();
+void print();
+
+int main(){
+	input();
+	return 0;
+}
+
+//Gets a line and separates it into tokens which are saved in a pointer array
+//It stops when the word 'exit' is given as input or when ctrl+D is pressed
+void input(){
+	char str[512];
+	const char s[] = "|><&; \t\n";
+	char *token;
+	int i;
+  	printf(">>");
+	while(fgets(str, sizeof(str), stdin) != NULL){
+		i = 0;
+		token = strtok(str, s);
+   		while( token != NULL ) {
+      			words[i++] = strdup(token);
+      			token = strtok(NULL, s);
+   		}
+		words[i] = NULL;
+		if(strcmp(words[0],"exit") == 0){
+			if(words[1] != NULL){
+				printf("Exit doesn't take any parameters.\n");
+				break;
+			}
+			else{
+				break;
+			}
+		}
+		print();
+   		printf(">>");
+	}
+}
+
+
+//Print the content of the pointer array
+void print(){
+	int i = 0;
+	printf("The list of saved tokens is: \n");
+	while(words[i]! = NULL){
+		printf("\"%s\"\n",words[i++]);
+	}
+}
