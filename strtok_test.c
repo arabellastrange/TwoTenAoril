249be64b3d26d4301d8f62d9f8149a54fe5fd190
Scott McGuire <gbb15159@uni.strath.ac.uk>
2017-02-24 13:30:06 +0000

    Added a couple of comments to the strtok_test.c file

--- strtok_test.c
+++ strtok_test.c
@@ -11,9 +11,11 @@ int main()
 
 void split_string()
 {
+	// If this is passed in it has to be an array, it can't be a pointer.
 	char command[] = "1:cd Desktop/Projects";
+
 	char *history_number 	= strtok(command, ":");
-	char *history_input 	= strtok(NULL, ":");
+	char *history_input 	= strtok(NULL, ":"); // The NULL tells strtok to continue where it left off.
 	
 	printf("%s\n", history_number);
 	printf("%s\n", history_input);
